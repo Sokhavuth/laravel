39ee4d906368b61112e76fa8ac9875890ad10034
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-06-24 11:31:02 +0700

    Make it better

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,6 +8,7 @@
     <form action="<?php echo url('/admin') ?>" method='post'>
         <a>Email: </a><input type="email" name="email" required />
         <a>ពាក្យ​សំងាត់ៈ</a><input type='password' name='password' required />
+        <a></a><input type='submit' value='បញ្ជូន' />
         <a></a><div class='message'>{{ $message }}</div>
     </form>
 </div>
